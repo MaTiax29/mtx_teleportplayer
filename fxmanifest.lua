8a6fcb50be90d1750ca9c137845381c772ae1466
MaTiax29 <77850782+MaTiax29@users.noreply.github.com>
2021-08-19 10:16:27 -0500

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author '!MaTiax29#5561'
+description 'A simple script that allows you to teleport to any of the locations marked in the config file.'
+version '1.0.0'
+
+client_script 'client/*.lua'
+
+shared_script 'shared/*.lua'
