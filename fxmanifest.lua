706b8a92b64dc170d939ade69bfe8b698a726f02
MaTiax29 <77850782+MaTiax29@users.noreply.github.com>
2021-08-21 15:18:32 -0500

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 
 author '!MaTiax29#5561'
 description 'A simple script that allows you to teleport to any of the locations marked in the config file.'
-version '1.0.0'
+version '1.1'
 
 client_script 'client.lua'
 
