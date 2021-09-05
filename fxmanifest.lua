2593da39cc6cf0f7a85543db4bb1a438ae6b942e
MaTiax29 <77850782+MaTiax29@users.noreply.github.com>
2021-09-04 23:47:33 -0500

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,10 +1,13 @@
 fx_version 'cerulean'
 game 'gta5'
 
-author '!MaTiax29#5561'
-description 'A simple script that allows you to teleport to any of the locations marked in the config file.'
-version '1.1'
+author 'MaTiax29'
+description 'A simple script that allows you to teleport to any of the locations marked in the configuration file using qtarget and nh-context.'
+version '1.0'
 
 client_script 'client.lua'
 
-shared_script 'config.lua'
+shared_scripts {
+    '@es_extended/imports.lua',
+    'config.lua'
+}
