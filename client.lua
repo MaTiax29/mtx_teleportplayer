52d1d0e996b16a5ed9d4d4860b52bfafaf0a0787
MaTiax29 <77850782+MaTiax29@users.noreply.github.com>
2021-09-08 21:36:23 -0500

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,22 +1,55 @@
+RegisterNetEvent('esx:playerLoaded') 
+AddEventHandler('esx:playerLoaded', function(xPlayer, isNew)
+    ESX.PlayerData = xPlayer
+    ESX.PlayerLoaded = true
+end)
+
+RegisterNetEvent('esx:playerLogout') 
+AddEventHandler('esx:playerLogout', function(xPlayer, isNew)
+    ESX.PlayerLoaded = false
+    ESX.PlayerData = {}
+end)
+
+RegisterNetEvent('esx:setJob')
+AddEventHandler('esx:setJob', function(job)
+    ESX.PlayerData.job = job
+end)
+
+AddEventHandler('onClientResourceStart', function(resource)
+    if resource == GetCurrentResourceName() then
+        SpawnPed()
+    end
+end)
+
+AddEventHandler('onResourceStop', function(resource)
+    if resource == GetCurrentResourceName() then
+        if ped then
+            DeletePed(ped)
+        end
+    end
+end)
+
+-- ########### -- 
+
 -- Threads
 Citizen.CreateThread(function()
-    local config = Config['Main']
-
-    for i = 1, #config, 1 do
-        local Part = i
-        local Mped = GetHashKey(config[i]['Model'])
-
-        exports['qtarget']:AddTargetModel({Mped}, {
+    for k,v in pairs(Config['Main']) do
+        exports['qtarget']:AddBoxZone('Teleport - '..k, v['Coords'], 0.6, 0.6, {
+            name = v['Label'],
+            heading = v['Heading'],
+            debugPoly = false,
+            minZ = v['ZCoords']['min'],
+            maxZ = v['ZCoords']['max'],
+        }, {
             options = {
                 {
                     event = 'MenuPoints',
-                    icon = 'fas fa-map-marked-alt',
-                    label = 'Teleport '..Part,
+                    icon = Config['Icon'],
+                    label = v['Label']
                 }
             },
-            distance = Config['Distance']
+            distance = Config['Distance'],
         })
-
     end
 end)
 
@@ -48,39 +81,6 @@ AddEventHandler('TeleportPlayer', function(data)
     end
 end)
 
-RegisterNetEvent('onClientResourceStart')
-AddEventHandler('onClientResourceStart', function(resource)
-    if GetCurrentResourceName() == resource then
-        SpawnPed()
-    end
-end)
-
-RegisterNetEvent('onResourceStop')
-AddEventHandler('onResourceStop', function(resource)
-    if GetCurrentResourceName() == resource then
-        if ped then
-            DeletePed(ped)
-        end
-    end
-end)
-
-RegisterNetEvent('esx:playerLoaded') 
-AddEventHandler('esx:playerLoaded', function(xPlayer, isNew)
-    ESX.PlayerData = xPlayer
-    ESX.PlayerLoaded = true
-end)
-
-RegisterNetEvent('esx:playerLogout') 
-AddEventHandler('esx:playerLogout', function(xPlayer, isNew)
-    ESX.PlayerLoaded = false
-    ESX.PlayerData = {}
-end)
-
-RegisterNetEvent('esx:setJob')
-AddEventHandler('esx:setJob', function(job)
-    ESX.PlayerData.job = job
-end)
-
 -- Functions
 function SpawnPed()
     for k,v in pairs(Config['Main']) do
@@ -89,8 +89,7 @@ function SpawnPed()
             Citizen.Wait(1)
         end
     
-        local x, y, z = table.unpack(v['Coords'])
-        ped = CreatePed(5, GetHashKey(v['Model']), x, y, z - 1, v['Heading'], false, true)
+        ped = CreatePed(5, GetHashKey(v['Model']), v['Coords'], v['Heading'], false, true)
         SetEntityAlpha(ped, 255, false)
         FreezeEntityPosition(ped, true) 
         SetEntityInvincible(ped, true) 
